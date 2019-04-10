7cc6fbd6665077acabc35adb502f45f91fa1ba2a
Yosuke Onoue <onoue@likr-lab.com>
2019-04-11 07:29:36 +0900

    [core] add node stroke support

--- packages/eg-renderer-core/src/renderer/meshes/circle_nodes.rs
+++ packages/eg-renderer-core/src/renderer/meshes/circle_nodes.rs
@@ -2,17 +2,23 @@ use super::program::{init_fragment_shader, init_program, init_vertex_shader};
 use super::{LayoutData, Mesh, VertexData};
 use web_sys::{WebGl2RenderingContext, WebGlBuffer, WebGlProgram, WebGlVertexArrayObject};
 
-const CIRCLE_NODE_ATTRIBUTES: usize = 20;
+const CIRCLE_NODE_ATTRIBUTES: usize = 32;
 
 const CIRCLE_NODE_VERTEX_SHADER_SOURCE: &str = r#"#version 300 es
-layout(location = 0) in vec2 aPosition0;
-layout(location = 1) in vec2 aPosition1;
-layout(location = 2) in vec4 aColor0;
-layout(location = 3) in vec4 aColor1;
-layout(location = 4) in vec2 aCenterPosition0;
-layout(location = 5) in vec2 aCenterPosition1;
-layout(location = 6) in vec2 aSize0;
-layout(location = 7) in vec2 aSize1;
+layout(location = 0) in float aAlpha0;
+layout(location = 1) in float aAlpha1;
+layout(location = 2) in vec2 aPosition0;
+layout(location = 3) in vec2 aPosition1;
+layout(location = 4) in vec4 aColor0;
+layout(location = 5) in vec4 aColor1;
+layout(location = 6) in vec2 aCenterPosition0;
+layout(location = 7) in vec2 aCenterPosition1;
+layout(location = 8) in vec2 aSize0;
+layout(location = 9) in vec2 aSize1;
+layout(location = 10) in vec4 aStrokeColor0;
+layout(location = 11) in vec4 aStrokeColor1;
+layout(location = 12) in float aStrokeWidth0;
+layout(location = 13) in float aStrokeWidth1;
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
 uniform float r;
@@ -20,14 +26,21 @@ out vec2 vPosition;
 out vec4 vColor;
 out vec2 vCenterPosition;
 out vec2 vSize;
+out vec4 vStrokeColor;
+out float vStrokeWidth;
 
 void main() {
+  float alpha = r * aAlpha1 + (1.0 - r) * aAlpha0;
   vPosition = r * aPosition1 + (1.0 - r) * aPosition0;
   vec4 mvPosition = uMVMatrix * vec4(vPosition, 1.0, 1.0);
   gl_Position = uPMatrix * mvPosition;
   vColor = r * aColor1 + (1.0 - r) * aColor0;
+  vColor.a *= alpha;
   vCenterPosition = r * aCenterPosition1 + (1.0 - r) * aCenterPosition0;
   vSize = r * aSize1 + (1.0 - r) * aSize0;
+  vStrokeColor = r * aStrokeColor1 + (1.0 - r) * aStrokeColor0;
+  vStrokeColor.a *= alpha;
+  vStrokeWidth = r * aStrokeWidth1 + (1.0 - r) * aStrokeWidth0;
 }
 "#;
 
@@ -37,15 +50,20 @@ in vec2 vPosition;
 in vec4 vColor;
 in vec2 vCenterPosition;
 in vec2 vSize;
+in vec4 vStrokeColor;
+in float vStrokeWidth;
 out vec4 oFragColor;
 void main() {
-  float a = vSize.y / vSize.x;
-  vec2 v = vPosition - vCenterPosition;
-  v.y /= a;
-  if (dot(v, v) > vSize.x * vSize.x / 4.0) {
+  vec2 v1 = 2.0 * (vPosition - vCenterPosition) / (vSize + vStrokeWidth);
+  if (dot(v1, v1) > 1.0) {
     discard;
   }
-  oFragColor = vColor;
+  vec2 v2 = 2.0 * (vPosition - vCenterPosition) / (vSize - vStrokeWidth);
+  if (dot(v2, v2) > 1.0) {
+    oFragColor = vStrokeColor;
+  } else {
+    oFragColor = vColor;
+  }
 }
 "#;
 
@@ -61,6 +79,8 @@ fn init_vertex_array(
     vertex_buffer: &WebGlBuffer,
     element_buffer: &WebGlBuffer,
 ) -> Result<WebGlVertexArrayObject, String> {
+    let alpha0_location = gl.get_attrib_location(program, "aAlpha0");
+    let alpha1_location = gl.get_attrib_location(program, "aAlpha1");
     let position0_location = gl.get_attrib_location(program, "aPosition0");
     let position1_location = gl.get_attrib_location(program, "aPosition1");
     let color0_location = gl.get_attrib_location(program, "aColor0");
@@ -69,6 +89,10 @@ fn init_vertex_array(
     let center_position1_location = gl.get_attrib_location(program, "aCenterPosition1");
     let size0_location = gl.get_attrib_location(program, "aSize0");
     let size1_location = gl.get_attrib_location(program, "aSize1");
+    let stroke_color0_location = gl.get_attrib_location(program, "aStrokeColor0");
+    let stroke_color1_location = gl.get_attrib_location(program, "aStrokeColor1");
+    let stroke_width0_location = gl.get_attrib_location(program, "aStrokeWidth0");
+    let stroke_width1_location = gl.get_attrib_location(program, "aStrokeWidth1");
     let array = gl
         .create_vertex_array()
         .ok_or("failed to create vertex array")?;
@@ -78,6 +102,8 @@ fn init_vertex_array(
         WebGl2RenderingContext::ELEMENT_ARRAY_BUFFER,
         Some(&element_buffer),
     );
+    gl.enable_vertex_attrib_array(alpha0_location as u32);
+    gl.enable_vertex_attrib_array(alpha1_location as u32);
     gl.enable_vertex_attrib_array(position0_location as u32);
     gl.enable_vertex_attrib_array(position1_location as u32);
     gl.enable_vertex_attrib_array(color0_location as u32);
@@ -86,69 +112,121 @@ fn init_vertex_array(
     gl.enable_vertex_attrib_array(center_position1_location as u32);
     gl.enable_vertex_attrib_array(size0_location as u32);
     gl.enable_vertex_attrib_array(size1_location as u32);
+    gl.enable_vertex_attrib_array(stroke_color0_location as u32);
+    gl.enable_vertex_attrib_array(stroke_color1_location as u32);
+    gl.enable_vertex_attrib_array(stroke_width0_location as u32);
+    gl.enable_vertex_attrib_array(stroke_width1_location as u32);
+    gl.vertex_attrib_pointer_with_i32(
+        alpha0_location as u32,
+        1,
+        WebGl2RenderingContext::FLOAT,
+        false,
+        128,
+        0,
+    );
+    gl.vertex_attrib_pointer_with_i32(
+        alpha1_location as u32,
+        1,
+        WebGl2RenderingContext::FLOAT,
+        false,
+        128,
+        4,
+    );
     gl.vertex_attrib_pointer_with_i32(
         position0_location as u32,
         2,
         WebGl2RenderingContext::FLOAT,
         false,
-        80,
-        0,
+        128,
+        8,
     );
     gl.vertex_attrib_pointer_with_i32(
         position1_location as u32,
         2,
         WebGl2RenderingContext::FLOAT,
         false,
-        80,
-        8,
+        128,
+        16,
     );
     gl.vertex_attrib_pointer_with_i32(
         color0_location as u32,
         4,
         WebGl2RenderingContext::FLOAT,
         false,
-        80,
-        16,
+        128,
+        24,
     );
     gl.vertex_attrib_pointer_with_i32(
         color1_location as u32,
         4,
         WebGl2RenderingContext::FLOAT,
         false,
-        80,
-        32,
+        128,
+        40,
     );
     gl.vertex_attrib_pointer_with_i32(
         center_position0_location as u32,
         2,
         WebGl2RenderingContext::FLOAT,
         false,
-        80,
-        48,
+        128,
+        56,
     );
     gl.vertex_attrib_pointer_with_i32(
         center_position1_location as u32,
         2,
         WebGl2RenderingContext::FLOAT,
         false,
-        80,
-        56,
+        128,
+        64,
     );
     gl.vertex_attrib_pointer_with_i32(
         size0_location as u32,
         2,
         WebGl2RenderingContext::FLOAT,
         false,
-        80,
-        64,
+        128,
+        72,
     );
     gl.vertex_attrib_pointer_with_i32(
         size1_location as u32,
         2,
         WebGl2RenderingContext::FLOAT,
         false,
+        128,
         80,
-        72,
+    );
+    gl.vertex_attrib_pointer_with_i32(
+        stroke_color0_location as u32,
+        4,
+        WebGl2RenderingContext::FLOAT,
+        false,
+        128,
+        88,
+    );
+    gl.vertex_attrib_pointer_with_i32(
+        stroke_color1_location as u32,
+        4,
+        WebGl2RenderingContext::FLOAT,
+        false,
+        128,
+        104,
+    );
+    gl.vertex_attrib_pointer_with_i32(
+        stroke_width0_location as u32,
+        1,
+        WebGl2RenderingContext::FLOAT,
+        false,
+        128,
+        120,
+    );
+    gl.vertex_attrib_pointer_with_i32(
+        stroke_width1_location as u32,
+        1,
+        WebGl2RenderingContext::FLOAT,
+        false,
+        128,
+        124,
     );
     Ok(array)
 }
@@ -235,84 +313,132 @@ impl CircleNodes {
         Ok(())
     }
 
-    fn set_current_x(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_alpha(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 0)
     }
 
-    fn set_current_y(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_alpha(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 1)
     }
 
-    fn set_next_x(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_x(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 2)
     }
 
-    fn set_next_y(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_y(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 3)
     }
 
-    fn set_current_r(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_x(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 4)
     }
 
-    fn set_current_g(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_y(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 5)
     }
 
-    fn set_current_b(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_fill_r(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 6)
     }
 
-    fn set_current_alpha(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_fill_g(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 7)
     }
 
-    fn set_next_r(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_fill_b(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 8)
     }
 
-    fn set_next_g(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_fill_alpha(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 9)
     }
 
-    fn set_next_b(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_fill_r(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 10)
     }
 
-    fn set_next_alpha(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_fill_g(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 11)
     }
 
-    fn set_current_center_x(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_fill_b(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 12)
     }
 
-    fn set_current_center_y(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_fill_alpha(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 13)
     }
 
-    fn set_next_center_x(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_center_x(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 14)
     }
 
-    fn set_next_center_y(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_center_y(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 15)
     }
 
-    fn set_current_width(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_center_x(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 16)
     }
 
-    fn set_current_height(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_next_center_y(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 17)
     }
 
-    fn set_next_width(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_width(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 18)
     }
 
-    fn set_next_height(&mut self, index: usize, value: f32) -> Result<(), String> {
+    fn set_current_height(&mut self, index: usize, value: f32) -> Result<(), String> {
         self.set_value(index, value, 19)
+    }
+
+    fn set_next_width(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 20)
+    }
+
+    fn set_next_height(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 21)
+    }
+
+    fn set_current_stroke_r(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 22)
+    }
+
+    fn set_current_stroke_g(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 23)
+    }
+
+    fn set_current_stroke_b(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 24)
+    }
+
+    fn set_current_stroke_alpha(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 25)
+    }
+
+    fn set_next_stroke_r(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 26)
+    }
+
+    fn set_next_stroke_g(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 27)
+    }
+
+    fn set_next_stroke_b(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 28)
+    }
+
+    fn set_next_stroke_alpha(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 29)
+    }
+
+    fn set_current_stroke_width(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 30)
+    }
+
+    fn set_next_stroke_width(&mut self, index: usize, value: f32) -> Result<(), String> {
+        self.set_value(index, value, 31)
     }
 
     fn set_item(
@@ -324,42 +450,44 @@ impl CircleNodes {
         a1: f32,
     ) -> Result<(), String> {
         let current_x = [
-            current.x - current.width / 2.,
-            current.x + current.width / 2.,
-            current.x - current.width / 2.,
-            current.x + current.width / 2.,
+            current.x - (current.width + current.strokeWidth) / 2.,
+            current.x + (current.width + current.strokeWidth) / 2.,
+            current.x - (current.width + current.strokeWidth) / 2.,
+            current.x + (current.width + current.strokeWidth) / 2.,
         ];
         let current_y = [
-            current.y - current.height / 2.,
-            current.y - current.height / 2.,
-            current.y + current.height / 2.,
-            current.y + current.height / 2.,
+            current.y - (current.height + current.strokeWidth) / 2.,
+            current.y - (current.height + current.strokeWidth) / 2.,
+            current.y + (current.height + current.strokeWidth) / 2.,
+            current.y + (current.height + current.strokeWidth) / 2.,
         ];
         let next_x = [
-            next.x - next.width / 2.,
-            next.x + next.width / 2.,
-            next.x - next.width / 2.,
-            next.x + next.width / 2.,
+            next.x - (next.width + next.strokeWidth) / 2.,
+            next.x + (next.width + next.strokeWidth) / 2.,
+            next.x - (next.width + next.strokeWidth) / 2.,
+            next.x + (next.width + next.strokeWidth) / 2.,
         ];
         let next_y = [
-            next.y - next.height / 2.,
-            next.y - next.height / 2.,
-            next.y + next.height / 2.,
-            next.y + next.height / 2.,
+            next.y - (next.height + next.strokeWidth) / 2.,
+            next.y - (next.height + next.strokeWidth) / 2.,
+            next.y + (next.height + next.strokeWidth) / 2.,
+            next.y + (next.height + next.strokeWidth) / 2.,
         ];
         for i in 0..4 {
+            self.set_current_alpha(index * 4 + i, a0)?;
+            self.set_next_alpha(index * 4 + i, a1)?;
             self.set_current_x(index * 4 + i, current_x[i] as f32)?;
             self.set_current_y(index * 4 + i, current_y[i] as f32)?;
             self.set_next_x(index * 4 + i, next_x[i] as f32)?;
             self.set_next_y(index * 4 + i, next_y[i] as f32)?;
-            self.set_current_r(index * 4 + i, (current.fillColor.r / 255.) as f32)?;
-            self.set_current_g(index * 4 + i, (current.fillColor.g / 255.) as f32)?;
-            self.set_current_b(index * 4 + i, (current.fillColor.b / 255.) as f32)?;
-            self.set_current_alpha(index * 4 + i, a0)?;
-            self.set_next_r(index * 4 + i, (next.fillColor.r / 255.) as f32)?;
-            self.set_next_g(index * 4 + i, (next.fillColor.g / 255.) as f32)?;
-            self.set_next_b(index * 4 + i, (next.fillColor.b / 255.) as f32)?;
-            self.set_next_alpha(index * 4 + i, a1)?;
+            self.set_current_fill_r(index * 4 + i, (current.fillColor.r / 255.) as f32)?;
+            self.set_current_fill_g(index * 4 + i, (current.fillColor.g / 255.) as f32)?;
+            self.set_current_fill_b(index * 4 + i, (current.fillColor.b / 255.) as f32)?;
+            self.set_current_fill_alpha(index * 4 + i, current.fillColor.opacity as f32)?;
+            self.set_next_fill_r(index * 4 + i, (next.fillColor.r / 255.) as f32)?;
+            self.set_next_fill_g(index * 4 + i, (next.fillColor.g / 255.) as f32)?;
+            self.set_next_fill_b(index * 4 + i, (next.fillColor.b / 255.) as f32)?;
+            self.set_next_fill_alpha(index * 4 + i, next.fillColor.opacity as f32)?;
             self.set_current_center_x(index * 4 + i, current.x as f32)?;
             self.set_current_center_y(index * 4 + i, current.y as f32)?;
             self.set_next_center_x(index * 4 + i, next.x as f32)?;
@@ -368,6 +496,16 @@ impl CircleNodes {
             self.set_current_height(index * 4 + i, current.height as f32)?;
             self.set_next_width(index * 4 + i, next.width as f32)?;
             self.set_next_height(index * 4 + i, next.height as f32)?;
+            self.set_current_stroke_r(index * 4 + i, (current.strokeColor.r / 255.) as f32)?;
+            self.set_current_stroke_g(index * 4 + i, (current.strokeColor.g / 255.) as f32)?;
+            self.set_current_stroke_b(index * 4 + i, (current.strokeColor.b / 255.) as f32)?;
+            self.set_current_stroke_alpha(index * 4 + i, current.strokeColor.opacity as f32)?;
+            self.set_next_stroke_r(index * 4 + i, (next.strokeColor.r / 255.) as f32)?;
+            self.set_next_stroke_g(index * 4 + i, (next.strokeColor.g / 255.) as f32)?;
+            self.set_next_stroke_b(index * 4 + i, (next.strokeColor.b / 255.) as f32)?;
+            self.set_next_stroke_alpha(index * 4 + i, next.strokeColor.opacity as f32)?;
+            self.set_current_stroke_width(index * 4 + i, current.strokeWidth as f32)?;
+            self.set_next_stroke_width(index * 4 + i, next.strokeWidth as f32)?;
         }
         Ok(())
     }
@@ -401,13 +539,7 @@ impl Mesh for CircleNodes {
             offset += 1;
         }
         for node in &layout.update.vertices {
-            self.set_item(
-                offset,
-                &node.current,
-                &node.next,
-                node.current.fillColor.opacity as f32,
-                node.next.fillColor.opacity as f32,
-            )?;
+            self.set_item(offset, &node.current, &node.next, 1., 1.)?;
             offset += 1
         }
         for node in &layout.exit.vertices {
